b38a33aef3f889685305c61051205b17ff1ec565
songpu2015617 <songpu2015617@users.noreply.github.com>
2016-09-05 00:27:27 -0400

    Add files via upload

---
+++ awesometodo/TodoListAdapter.java
@@ -0,0 +1,68 @@
+package com.pittsburgh.pusong.awesometodo;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.pittsburgh.pusong.awesometodo.models.Todo;
+
+import java.util.List;
+
+public class TodoListAdapter extends BaseAdapter {
+
+    private Context context;
+    private List<Todo> data;
+
+    public TodoListAdapter(@NonNull Context context, List<Todo> data) {
+        this.context = context;
+        this.data = data;
+    }
+
+    @Override
+    public int getCount() {
+        return data.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return data.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder vh;
+        if (convertView == null) {
+
+            Log.d("guojing_todo", position + "");
+
+            convertView = LayoutInflater.from(context).inflate(R.layout.main_list_item, parent, false);
+
+            vh = new ViewHolder();
+            // we only find once
+            vh.todoText = (TextView) convertView.findViewById(R.id.main_list_item_text);
+            // cache the view holder
+            convertView.setTag(vh);
+        } else {
+            // convertView is not null, which means it contains a cached view holder
+            vh = (ViewHolder) convertView.getTag();
+        }
+
+        Todo todo = data.get(position);
+        vh.todoText.setText(todo.text);
+        return convertView;
+    }
+
+    private static class ViewHolder {
+        TextView todoText;
+    }
+}
